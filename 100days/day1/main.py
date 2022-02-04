bea7df56d0442071366dc37696a35e77da3067ba
khushpatil <khushpatil70@gmail.com>
2022-02-04 12:30:58 +0530

    The beginning of a 100 days of code

---
+++ 100days/day1/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name creator !")
+name =input("What is the name of your hometown?: \n")
+pet = input("What is the name of your pet?: \n")
+print("The name of your band is " + name + " " + pet)
+print("Thank you for using our tool!")
